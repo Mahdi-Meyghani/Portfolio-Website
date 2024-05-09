e5f671990a0873e3a673981c53f8b08765c47975
Mahdi <mahdimeyghani02@gmail.com>
2024-05-09 18:41:49 +0330

    Improve Application

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 col3, col4 = st.columns(2)
 
+
+
 # background-image: linear-gradient( 179.7deg,  rgba(249,21,215,1) 1.1%, rgba(22,0,98,1) 99% );
 # background-image: linear-gradient( 85.2deg,  rgba(33,3,40,1) 7.5%, rgba(65,5,72,1) 88.7% );
 
@@ -36,7 +38,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 with col1:
-    st.image("me.jpg")
+    st.image("pictures/me.jpg")
 
 with col2:
     st.markdown("<h1 class='slide-in-left'> Mahdi Meyghani</h1>",
@@ -61,12 +63,14 @@ with col2:
     # below you can see some of my projects."""
     # st.info(content)
 
-    st.write("""<font size='+5'>Hi, I am Mahdi.
+    st.write("""<font size='+4'><p class='slide-in-left'>Hi, I am Mahdi.
     I am a Python programmer focusing on backend.
     I also have a background in graphics, but it doesn't matter because right now
     my passion and all my time is spent on technology, anyway below you can find
     some of the Apps
-    i have built in Python, Feel free to contact me !</font>""", unsafe_allow_html=True)
+    i have built in Python, Feel free to contact me !</font></p>""", unsafe_allow_html=True)
+
+    st.image("pictures/arrow.png")
 with col3:
     st.markdown("<h1 class='slide-in-left'> To Do App</h1>",
                 unsafe_allow_html=True)
@@ -74,7 +78,7 @@ with col3:
     st.write("""A distraction-free web app
     to help you focus on your goals :)""", )
 
-    st.image("1.png")
+    st.image("pictures/1.png")
 
     st.write("[Source Code](https://github.com/Mahdi-Meyghani)")
     # --------------------------------------------------------------------------
@@ -84,7 +88,7 @@ with col3:
     st.write("""A distraction-free web app
     to help you focus on your goals :)""", )
 
-    st.image("1.png")
+    st.image("pictures/1.png")
 
     st.write("[Source Code](https://github.com/Mahdi-Meyghani)")
 
@@ -95,7 +99,7 @@ with col4:
     st.write("""A portfolio website built
     entirely in Python to showcase coding projects.""")
 
-    st.image("2.png")
+    st.image("pictures/2.png")
 
     st.write("[Source code](https://github.com/Mahdi-Meyghani)")
     # --------------------------------------------------------------------------
@@ -105,6 +109,6 @@ with col4:
     st.write("""A portfolio website built
     entirely in Python to showcase coding projects.""")
 
-    st.image("2.png")
+    st.image("pictures/2.png")
 
     st.write("[Source code](https://github.com/Mahdi-Meyghani)")
