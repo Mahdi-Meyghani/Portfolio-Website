32a405b9259e28ace00f8dc9c0203505206f7ac7
Mahdi <mahdimeyghani02@gmail.com>
2024-05-11 22:58:03 +0330

    Create backend file for send email

---
+++ sending_email.py
@@ -0,0 +1,29 @@
+import smtplib
+import ssl
+
+
+def send_email(email_address: str, message: str):
+    user = "mahdimeyghani02@gmail.com"
+    password = "xbce ueit fxhd ynxh"
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    context = ssl.create_default_context()
+
+    email_sender = email_address
+    email_receiver = "mahdimeyghani02@gmail.com"
+
+    subject = "Portfolio Website"
+
+    body = message
+
+    email_message = f"subject: {subject}\n\nfrom: {email_sender}\n{body}"
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user, password)
+        server.sendmail(user, email_receiver, email_message)
+
+
+if __name__ == "__main__":
+    send_email("boochip02@gmail.com", "Hello\nHows everything?\nIs everything ok?")
